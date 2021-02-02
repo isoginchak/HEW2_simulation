e6e21b9dcd8b2d69021d7fed41d365013b900215
minatsu <isoginchak825@gmail.com>
2021-02-03 02:18:24 +0900

    パスケース追加/name変更なし

---
+++ hew2.js
@@ -0,0 +1,15 @@
+const express =require('express');
+const ejs = require('ejs');
+const app = express();
+const path = require("path");
+
+app.set('views', __dirname + '/views');
+app.set('views engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('passcase_customize.ejs');
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use('/img', express.static('img'));
+app.listen(3000);
